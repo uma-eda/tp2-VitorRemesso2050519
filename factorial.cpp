fb004b035d5295ebd5ad8e145aecd0e8d760847e
VitorRemesso2050519 <2050519@student.uma.pt>
2020-02-24 12:03:17 +0000

    Texto.

--- factorial.cpp
+++ factorial.cpp
@@ -1,7 +1,16 @@
-int factorial (int n)
+#include <iostream>
+
+using namespace std;
+
+int factorial(int n)
 {
    if (n == 1)
       return 1;
    else 
       return n * factorial(n - 1);
  }
+
+int main() {
+    cout << factorial(5);
+    return 0;
+}
